7a22c3d5795f317816a0297850ebf3770f81826a
Nathan Krasney <natankrasney@gmail.com>
2022-08-10 10:32:41 +0300

    Image with fill

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import Pets from "./pets";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Link href="/pets">
-        Navigate to Pets
-      </Link>
+      <Link href="/pets">Navigate to Pets</Link>
+      <p>Show image with fill</p>
+      <div style={{ width: 300, height: 400 , position: 'relative' }}>
+        <Image alt="pet-with-fill" layout="fill" src="/images/1.jpg" />
+      </div>
     </div>
   );
 };
